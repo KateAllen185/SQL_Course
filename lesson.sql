db45d9a6914891230526d0bd6f47895fdf43b101
KateAllen185 <kate.allen@uhbw.nhs.uk>
2026-09-11 13:02:27 +0000

    basic query where or statement

--- lesson.sql
+++ lesson.sql
@@ -1,8 +1,18 @@
 /*
 Example lesson stub
 */
+SELECT ps.Patientid,
+       ps.Hospital,
+       ps.ward,
+       ps.AdmittedDate,
+       ps.DischargeDate,
+       ps.Ethnicity
+FROM   PatientStay AS ps
+WHERE  ps.Hospital IN ('Kingston', 'PRUH')
+       AND (ps.Ward LIKE '%o%'
+            OR ps.Ward LIKE '%surgery')
+       AND ps.Ethnicity IS NULL;
 
-SELECT
-	*
-FROM
-	PatientStay ps ;
+
+/*SELECT distinct ps.Hospital
+	FROM PatientStay ps*/
