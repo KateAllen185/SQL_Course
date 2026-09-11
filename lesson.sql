3002e8e7f0c911740cf93b767132284c1ea54258
KateAllen185 <kate.allen@uhbw.nhs.uk>
2026-09-11 15:08:42 +0000

    End of course

--- lesson.sql
+++ lesson.sql
@@ -6,12 +6,26 @@ SELECT ps.Patientid,
        ps.ward,
        ps.AdmittedDate,
        ps.DischargeDate,
+       DATEADD(MONTH, 3, ps.DischargeDate) AS AppointmentDate,
+       DATEADD(WEEK, -2, ps.AdmittedDate) AS ReminderDate,
        ps.Ethnicity
 FROM   PatientStay AS ps
 WHERE  ps.Hospital IN ('Kingston', 'PRUH')
        AND (ps.Ward LIKE '%o%'
             OR ps.Ward LIKE '%surgery')
-       AND ps.Ethnicity IS NULL;
+       --AND ps.Ethnicity IS NULL
+       AND ps.AdmittedDate <= '2024-02-28'
+       ORDER BY ps.Hospital, ps.Ward
+
+     SELECT
+     ps.hospital,
+     ps.ward,
+     COUNT (*) AS NumberOfPatients,
+     SUM(ps.Tariff) as TotalTariff
+     
+     
+     FROM PatientStay AS ps
+     GROUP BY ps.Hospital, ps.Ward
 
 
 /*SELECT distinct ps.Hospital
